bd38031e76b5cd6de85ffcd0a99aa27822c87a44
Rodrigo Ferreira <rodrigo_ferreira89@outlook.com>
2022-11-06 18:45:14 -0300

    teste primeito commit utilizando a linguagem java e o eclipse

---
+++ src/aplicacao/Soma.java
@@ -0,0 +1,16 @@
+package aplicacao;
+
+public class Soma {
+
+	public static void main(String[] args) {
+		double valor1 = 3, valor2 = 2;
+		double soma;
+		
+		soma = valor1 + valor2;
+		
+		System.out.println(soma);
+		
+		
+	}
+
+}
