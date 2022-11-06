51cc360a33c4c7862205302d73788804bff407b2
Rodrigo Ferreira <rodrigo_ferreira89@outlook.com>
2022-11-06 18:48:38 -0300

    Segundo commit

--- src/aplicacao/Soma.java
+++ src/aplicacao/Soma.java
@@ -3,12 +3,19 @@ package aplicacao;
 public class Soma {
 
 	public static void main(String[] args) {
-		double valor1 = 3, valor2 = 2;
+		double valor1 = 3, valor2 = 5;
 		double soma;
 		
 		soma = valor1 + valor2;
 		
-		System.out.println(soma);
+		if (soma < 4) {
+			System.out.println(soma);
+		} else {
+			System.out.println("Hello word");
+		}
+		
+		
+		
 		
 		
 	}
